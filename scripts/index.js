dc0141332883375278f54d84dba865fee89340be
Barbara Step <b.barbarastep@gmail.com>
2025-06-03 15:18:53 +0300

    func create & delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
-// @todo: Темплейт карточки
+// Создание карточки
+function createCard(data, onDelete) {
+  const card = document.querySelector('#card-template').content.querySelector('.card').cloneNode(true);
+  const image = card.querySelector('.card__image');
+  const buttonDelete = card.querySelector('.card__delete-button');
+  const litle = card.querySelector('.card__title');
 
-// @todo: DOM узлы
+  litle.textContent = data.name;
+  image.src = data.link;
+  image.alt = data.name;
 
-// @todo: Функция создания карточки
+  buttonDelete.addEventListener('click', () => {
+    onDelete(card);
+  });
 
-// @todo: Функция удаления карточки
+  return card;
+}
 
-// @todo: Вывести карточки на страницу
+const cardList = document.querySelector('.places__list');
+
+// Удаление карточки
+function deleteCard(element) {
+  element.remove();
+}
+
+initialCards.forEach(data => {
+  const card = createCard(data, deleteCard);
+  cardList.appendChild(card);
+});
