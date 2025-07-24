29bc5373774e7210c9a0e2063850d0e7fbb1071b
Barbara Step <b.barbarastep@gmail.com>
2025-07-24 18:24:12 +0300

    feat: load cards with likes, enable deleting own cards, and update avatar via popup

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,31 +1,50 @@
+import { toggleCardLike, deleteCard } from './api.js';
+
 const cardTemplate = document.querySelector('#card-template').content.querySelector('.card');
 
 // Удаление карточки
-export function deleteCard(cardElement) {
-  cardElement.remove();
-}
-
-// Лайк карточки
-export function toggleCardLike(buttonLike) {
-  buttonLike.addEventListener('click', () => {
-    buttonLike.classList.toggle('card__like-button_is-active');
-  });
+export function handleDelete(cardElement, cardId) {
+  deleteCard(cardId)
+    .then(() => {
+      cardElement.remove();
+    })
+    .catch((err) => console.log('Ошибка при удалении карточки:', err));
 }
 
 // Создание карточки
-export function createCard(data, handleDelete, handleLike, handleImageClick) {
+export function createCard(data, handleDelete, handleImageClick, currentUserId) {
   const cardElement = cardTemplate.cloneNode(true);
   const imageCard = cardElement.querySelector('.card__image');
   const buttonDeleteCard = cardElement.querySelector('.card__delete-button');
   const titleCard = cardElement.querySelector('.card__title');
   const buttonLikeCard = cardElement.querySelector('.card__like-button');
+  const cardLikeCounter = cardElement.querySelector('.card__like-counter');
 
   titleCard.textContent = data.name;
   imageCard.src = data.link;
   imageCard.alt = data.name;
+  cardLikeCounter.textContent = data.likes.length;
 
-  buttonDeleteCard.addEventListener('click', () => handleDelete(cardElement));
-  handleLike(buttonLikeCard);
+  if (data.likes.some(user => user._id === currentUserId)) {
+  buttonLikeCard.classList.add('card__like-button_is-active');
+  }
+
+  if (data.owner._id !== currentUserId) {
+  buttonDeleteCard.remove();
+  }
+
+  buttonDeleteCard.addEventListener('click', () => handleDelete(cardElement, data._id));
+  buttonLikeCard.addEventListener('click', () => {
+    const isLiked = buttonLikeCard.classList.contains('card__like-button_is-active');
+
+    toggleCardLike(data._id, isLiked)
+      .then((updatedCard) => {
+        cardLikeCounter.textContent = updatedCard.likes.length;
+        buttonLikeCard.classList.toggle('card__like-button_is-active');
+      })
+      .catch((err) => console.log('Ошибка при лайке:', err));
+  });
+
   imageCard.addEventListener('click', () => handleImageClick(data));
 
   return cardElement;
